55cabde56e030444177f277c8bf84d1e9ccf1e2f
Gabriel Andretta <ohhgabriel@gmail.com>
2012-12-23 23:09:05 -0300

    Build a prefilled operation from the request

--- app/models/integration_request.rb
+++ app/models/integration_request.rb
@@ -2,4 +2,17 @@ class IntegrationRequest < ActiveRecord::Base
   has_one :operation
 
   serialize :raw
+
+  def build_prefilled_operation
+    build_operation(prefilled_operation_attributes)
+  end
+
+private
+
+  def prefilled_operation_attributes
+    {
+      buyer_phone: raw['buyer_phone'],
+      buyer_email: raw['buyer_email']
+    }
+  end
 end
--- test/models/integration_request_test.rb
+++ test/models/integration_request_test.rb
@@ -6,4 +6,15 @@ class IntegrationRequestTest < ActiveSupport::TestCase
     integration_request = IntegrationRequest.create!(raw: raw)
     assert_equal raw, integration_request.reload.raw
   end
+
+  test "builds a prefilled operation from it's raw attribute" do
+    integration_request = IntegrationRequest.new raw: {
+      'buyer_phone' => '0303456',
+      'buyer_email' => 'jerry@seinfeld.com'
+    }
+
+    operation = integration_request.build_prefilled_operation
+    assert_equal '0303456', operation.buyer_phone
+    assert_equal 'jerry@seinfeld.com', operation.buyer_email
+  end
 end
