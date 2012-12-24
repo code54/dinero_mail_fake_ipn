afd4f5722330f9fc6b0323c4ff645ce05567ab59
Gabriel Andretta <ohhgabriel@gmail.com>
2012-12-24 13:18:10 -0300

    Add delays to transition

--- app/models/transition.rb
+++ app/models/transition.rb
@@ -1,3 +1,8 @@
 class Transition < ActiveRecord::Base
   belongs_to :operation
+
+  validates :notification_delay, numericality: { allow_nil: true, only_integer: true, greater_than_or_equal_to: 0 }
+  validates :schedule_delay, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  attr_accessor :notification_delay, :schedule_delay
 end
--- test/models/transition_test.rb
+++ test/models/transition_test.rb
@@ -1,10 +1,57 @@
 require 'test_helper'
 
 class TransitionTest < ActiveSupport::TestCase
-  test "the truth" do
+  test "schedule delay validation" do
     transition = Transition.new
-    transition.save!
-    assert_not_nil transition.scheduled_at
-    puts transition.scheduled_at
+    transition.valid?
+    assert transition.errors[:schedule_delay].any?
+
+    transition.schedule_delay = 'abc'
+    assert_equal 'abc', transition.schedule_delay
+    transition.valid?
+    assert transition.errors[:schedule_delay].any?
+
+    transition.schedule_delay = 1.1
+    transition.valid?
+    assert transition.errors[:schedule_delay].any?
+
+    transition.schedule_delay = -1
+    transition.valid?
+    assert transition.errors[:schedule_delay].any?
+
+    transition.schedule_delay = 0
+    transition.valid?
+    assert transition.errors[:schedule_delay].empty?
+
+    transition.schedule_delay = 10
+    transition.valid?
+    assert transition.errors[:schedule_delay].empty?
+  end
+
+  test "notification delay validation" do
+    transition = Transition.new
+    transition.valid?
+    assert transition.errors[:notification_delay].empty?
+
+    transition.notification_delay = 'abc'
+    assert_equal 'abc', transition.notification_delay
+    transition.valid?
+    assert transition.errors[:notification_delay].any?
+
+    transition.notification_delay = 1.1
+    transition.valid?
+    assert transition.errors[:notification_delay].any?
+
+    transition.notification_delay = -1
+    transition.valid?
+    assert transition.errors[:notification_delay].any?
+
+    transition.notification_delay = 0
+    transition.valid?
+    assert transition.errors[:notification_delay].empty?
+
+    transition.notification_delay = 10
+    transition.valid?
+    assert transition.errors[:notification_delay].empty?
   end
 end
