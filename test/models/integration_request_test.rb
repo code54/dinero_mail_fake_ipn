2319adac8a1e710cf8fa02aa4fdbf85ac824950b
Gabriel Andretta <ohhgabriel@gmail.com>
2012-12-23 23:27:34 -0300

    Add an usable integration request fixture

--- test/models/integration_request_test.rb
+++ test/models/integration_request_test.rb
@@ -8,13 +8,10 @@ class IntegrationRequestTest < ActiveSupport::TestCase
   end
 
   test "builds a prefilled operation from it's raw attribute" do
-    integration_request = IntegrationRequest.new raw: {
-      'buyer_phone' => '0303456',
-      'buyer_email' => 'jerry@seinfeld.com'
-    }
+    integration_request = integration_requests(:jacket)
 
     operation = integration_request.build_prefilled_operation
-    assert_equal '0303456', operation.buyer_phone
-    assert_equal 'jerry@seinfeld.com', operation.buyer_email
+    assert_equal integration_request.raw['buyer_phone'], operation.buyer_phone
+    assert_equal integration_request.raw['buyer_email'], operation.buyer_email
   end
 end
