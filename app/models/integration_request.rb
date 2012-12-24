da03b9500ff6d1c1abd40862e38ebe93e22772c1
Gabriel Andretta <ohhgabriel@gmail.com>
2012-12-23 23:52:43 -0300

    Build operations with user provided parameters

--- app/models/integration_request.rb
+++ app/models/integration_request.rb
@@ -3,16 +3,43 @@ class IntegrationRequest < ActiveRecord::Base
 
   serialize :raw
 
+  def build_operation(attributes = {})
+    super(attributes.merge(unchangeable_operation_attributes))
+  end
+
   def build_prefilled_operation
     build_operation(prefilled_operation_attributes)
   end
 
 private
 
+  def unchangeable_operation_attributes
+    {
+      client_id: raw['transaction_id'],
+      items: items_attributes.map { |item_attributes| Item.new(item_attributes) }
+    }
+  end
+
   def prefilled_operation_attributes
     {
       buyer_phone: raw['buyer_phone'],
       buyer_email: raw['buyer_email']
     }
+  end
+
+  def items_attributes
+    result = []
+    raw.each do |key, value|
+      if (match = key.match(/item_(name|ammount|quantity)_(\d+)/))
+        index = match.captures[1].to_i - 1
+        name = match.captures[0]
+        if name == 'ammount'
+          name = 'amount'
+          value = value.to_i / 100.0
+        end
+        (result[index] ||= {})[name] = value
+      end
+    end
+    result
   end
 end
--- test/models/integration_request_test.rb
+++ test/models/integration_request_test.rb
@@ -13,5 +13,33 @@ class IntegrationRequestTest < ActiveSupport::TestCase
     operation = integration_request.build_prefilled_operation
     assert_equal integration_request.raw['buyer_phone'], operation.buyer_phone
     assert_equal integration_request.raw['buyer_email'], operation.buyer_email
+    assert_equal integration_request.raw['transaction_id'], operation.client_id
+
+    assert_equal 1, operation.items.size
+    item = operation.items.first
+
+    assert_equal integration_request.raw['item_name_1'], item.name
+    assert_equal integration_request.raw['item_quantity_1'].to_i, item.quantity
+    assert_equal integration_request.raw['item_ammount_1'].to_i / 100.0, item.amount
+  end
+
+  test "builds an operation with user provided attributes" do
+    integration_request = integration_requests(:jacket)
+
+    operation = integration_request.build_operation(
+      buyer_phone: '4444',
+      client_id: '333'
+    )
+    assert_equal '4444', operation.buyer_phone
+    assert_nil operation.buyer_email
+    assert_equal integration_request.raw['transaction_id'], operation.client_id
+    assert_not_equal '333', operation.client_id
+
+    assert_equal 1, operation.items.size
+    item = operation.items.first
+
+    assert_equal integration_request.raw['item_name_1'], item.name
+    assert_equal integration_request.raw['item_quantity_1'].to_i, item.quantity
+    assert_equal integration_request.raw['item_ammount_1'].to_i / 100.0, item.amount
   end
 end
