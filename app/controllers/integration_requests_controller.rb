0f206f4dbbe7fc46730cdfa281a66120ed374a01
Gabriel Andretta <ohhgabriel@gmail.com>
2012-12-23 22:18:07 -0300

    Accept integration requests

--- app/controllers/integration_requests_controller.rb
+++ app/controllers/integration_requests_controller.rb
@@ -1,2 +1,15 @@
 class IntegrationRequestsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def create
+    @integration_request = IntegrationRequest.create!(integration_request_attributes)
+    # TODO: redirect to the checkout form.
+    render text: 'success'
+  end
+
+private
+
+  def integration_request_attributes
+    { raw: params.except(*request.path_parameters.keys) }
+  end
 end
--- test/controllers/integration_requests_controller_test.rb
+++ test/controllers/integration_requests_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class IntegrationRequestsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "integration request creation" do
+    params = { 'transaction_id' => '1' }
+
+    assert_difference ->{ IntegrationRequest.count } do
+      post :create, params
+    end
+
+    assert_response :success
+    assert_equal 'success', response.body
+
+    integration_request = assigns(:integration_request)
+    assert integration_request
+    assert_equal params, integration_request.raw
+  end
 end
