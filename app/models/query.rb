388b686ad411c1a52710867d5fbdd678a605f2c8
Gabriel Andretta <ohhgabriel@gmail.com>
2013-02-07 12:40:03 -0300

    Identify the queried transactions

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,2 +1,22 @@
 class Query < ActiveRecord::Base
+
+  def transaction_ids
+    find_transaction_ids_in_question_hash
+  end
+
+private
+
+  def find_transaction_ids_in_question_hash
+    # TODO: understand how Nori works, think if is worth failing louder when the
+    # question document is invalid, replace Nori, I don't really know right now.
+    # What I do know is that this is ugly.
+    Array(question_hash['reporte']['detalle']['consulta']['operaciones']['id'])
+  rescue NoMethodError
+    []
+  end
+
+  def question_hash
+    parser = Nori.new(convert_tags_to: ->(tag){ tag.downcase })
+    parser.parse(question_document)
+  end
 end
--- test/models/query_test.rb
+++ test/models/query_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
-class QueryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class QueryTransactionIdentificationTest < ActiveSupport::TestCase
+  def question_document(name)
+    path = Rails.root.join('test', 'fixtures', 'files', 'question_documents', "#{name}.xml")
+    File.read(path)
+  end
+
+  test "document with several ids" do
+    query = Query.new(question_document: question_document(:several_ids))
+    assert_equal %w[1 2 3 6], query.transaction_ids
+  end
+
+  test "document with one id" do
+    query = Query.new(question_document: question_document(:one_id))
+    assert_equal %w[7], query.transaction_ids
+  end
+
+  test "downcased document" do
+    query = Query.new(question_document: question_document(:downcased))
+    assert_equal %w[1 2], query.transaction_ids
+  end
+
+  test "document missing 'id' tag" do
+    query = Query.new(question_document: question_document(:missing_id))
+    assert_equal [], query.transaction_ids
+  end
+
+  test "document missing 'operaciones' tag" do
+    query = Query.new(question_document: question_document(:missing_operaciones))
+    assert_equal [], query.transaction_ids
+  end
+
+  test "document missing 'consulta' tag" do
+    query = Query.new(question_document: question_document(:missing_consulta))
+    assert_equal [], query.transaction_ids
+  end
+
+  test "document missing 'detalle' tag" do
+    query = Query.new(question_document: question_document(:missing_detalle))
+    assert_equal [], query.transaction_ids
+  end
+
+  test "document missing 'reporte' tag" do
+    query = Query.new(question_document: question_document(:missing_reporte))
+    assert_equal [], query.transaction_ids
+  end
+
+  test "random document" do
+    query = Query.new(question_document: question_document(:random))
+    assert_equal [], query.transaction_ids
+  end
+
+  test "no xml document" do
+    query = Query.new(question_document: question_document(:no_xml))
+    assert_equal [], query.transaction_ids
+  end
 end
